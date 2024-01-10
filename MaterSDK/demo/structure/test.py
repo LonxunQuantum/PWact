5369dca1ee90516e75bb065e5c55f75a8251bf78
WuXing <wuxingxing20g@ict.ac.cn>
2024-01-10 10:19:54 +0800

    rm MaterSDK

--- MaterSDK/demo/structure/test.py
+++
@@ -1,7 +0,0 @@
-from matersdk.io.publicLayer.structure import DStructure
-
-structure = DStructure.from_file(
-                file_format="pwmat", 
-                file_path="/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config")
-supercell = structure.make_supercell_(scaling_matrix=[3, 3, 1])
-print(supercell.cart_coords)
--- MaterSDK/source/descriptor/deepmd/test/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-model = torch.jit.load("/data/home/liuhanyu/hyliu/code/mlff/PWmatMLFF_dev/test/CH4_torch_script/torch_script_module.pt", map_location=torch.device("cuda:0"))
-print(model.davg[:, :4])
-print(model.dstd[:, :4])
