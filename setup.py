a18256991eb370391d7f982c3cec2058a6ef721b
WuXing <stars_sparkling@163.com>
2025-08-22 15:52:56 +0800

    version 0.4.1: run with lmps.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pwact", 
-    version="0.4.0.dev1",
+    version="0.4.1",
     author="LonxunQuantum",
     author_email="lonxun@pwmat.com",
     description="PWACT is an open-source automated active learning platform based on MatPL for efficient data sampling.",
